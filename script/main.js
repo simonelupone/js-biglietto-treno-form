f7e1698677c6851690281b388de94a70162cc68c
simonelupone <simone.lupone@gmail.com>
2025-03-26 21:02:43 +0100

    [DONE]: add funtion to clear the form inputs update condition to visualize the discount badge add style

--- script/main.js
+++ script/main.js
@@ -5,24 +5,44 @@ const overDiscount = 0.6;
 
 // definisco gli elementi della pagina che mi servono
 const distanceInput = document.getElementById('distance-field');
-const userAge = document.getElementById('userAge-field');
-const button = document.getElementById('button');
+const userAge = document.getElementById('user-age-field');
+const userName = document.getElementById('name-field');
+const userLastName = document.getElementById('last-name-field');
+
+const button = document.querySelector('.button');
+const buttonReset = document.querySelector('.button-reset');
 // console.log(distanceInput, userAge);
-const distance = document.getElementById('distance');
-const age = document.getElementById('age');
-const price = document.getElementById('price');
+
+const distance = document.getElementById('user-distance');
+const age = document.getElementById('user-age');
+const name = document.getElementById('user-name');
+const lastName = document.getElementById('user-last-name');
+const price = document.getElementById('user-ticket-price');
+const stockPrice = document.getElementById('stock-price');
+const badge = document.querySelector('.badge')
 
 // prendo il valore degli input
 const kmValue = distanceInput.value;
 const ageValue = userAge.value;
+const nameValue = userName.value;
+const lastNameValue = userLastName.value;
+
 
 // calcolo il prezzo del biglietto
 const ticketPrice = ticketStockPrice * kmValue;
 // console.log(ticketPrice);
 const finalPrice = (age) => {
     if (age < 18) {
+        badge.classList.add('text-bg-info');
+        badge.classList.remove('d-none');
+        badge.innerHTML = 'Sconto Young';
+        stockPrice.innerHTML = (ticketStockPrice * underDiscount).toFixed(2) + ' €';
         return ticketPrice * underDiscount;
     } else if (age > 65) {
+        badge.classList.add('text-bg-warning');
+        badge.classList.remove('d-none');
+        badge.innerHTML = 'Sconto Senior';
+        stockPrice.innerHTML = (ticketStockPrice * overDiscount).toFixed(2) + ' €';
         return ticketPrice * overDiscount;
     } else {
         return ticketPrice;
@@ -34,8 +54,19 @@ button.addEventListener('click', function (event) {
 
     distance.innerHTML = kmValue;
     age.innerHTML = ageValue;
+    name.innerHTML = nameValue;
+    lastName.innerHTML = lastNameValue;
+    stockPrice.innerHTML = ticketStockPrice + ' €';
 
     // visualizzo il prezzo del biglietto
-    price.innerHTML = Math.floor(finalPrice(ageValue) * 100) / 100 + ' €';
-    console.log(finalPrice(ageValue));
+    price.innerHTML = finalPrice(ageValue).toFixed(2) + ' €';
+    // console.log(finalPrice(ageValue));
+});
+
+buttonReset.addEventListener('click', function (event) {
+    distanceInput.innerHTML = '';
+    userAge.innerHTML = '';
+    userName.innerHTML = '';
+    userLastName.innerHTML = '';
+
 });
